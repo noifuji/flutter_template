2716b715039e27ac0f3da0b5804df16c2543d8c0
noifuji <ryoma1025@gmail.com>
2024-01-27 14:44:19 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class MyAppState extends State<MyApp> {
           //初期ロード中のロード画面
           return const MaterialApp(home: Center());
         } else if (dataSnapshot.error != null) {
-          //初期ロードに失敗した場合に表示するエラー表示
+          //初期ロードに失敗した場合に表示するエラー画面
           return MaterialApp(
             home: Scaffold(
               body: Center(
